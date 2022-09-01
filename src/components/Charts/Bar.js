61b453d93f201c42caaf3fe8183895e48a0d6aa6
Gabriele Scalici <gaba41@gmail.com>
2022-09-01 23:14:28 +0200

    fix: linear gaps between empty values

--- src/components/Charts/Bar.js
+++ src/components/Charts/Bar.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import fetchData from '../../lib/fetchData';
-import targets from '../../data/targets.json'
-import { format } from 'date-fns';
+import React, { useState, useEffect } from "react";
+import fetchData from "../../lib/fetchData";
+import targets from "../../data/targets.json";
+import { format } from "date-fns";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,124 +10,144 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
-export default function BarChart({mode, istat, latest}){
+export default function BarChart({ mode, istat, latest }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState({});
 
-    const [isLoading, setIsLoading] = useState(true)
-    const [data, setData] = useState({});
+  useEffect(() => {
+    async function get() {
+      const res = await fetchData(mode, istat, latest);
+      if (mode === "incidenza") {
+        setData({
+          date: res.map((a) => format(new Date(a.data), "dd/MM/yy")),
+          casi: res.map((a) => a.casi),
+          incidenza: res.map((a) => a.incidenza),
+        });
+      } else if (mode === "vaccini") {
+        const target = targets.filter((a) => a.pro_com_t.endsWith(istat))[0];
+        const date = res.map((a) => format(new Date(a.data), "dd/MM/yy"));
+        const vaccinati = res
+          .map((a) =>
+            new Date(a.data) > new Date("2021-12-08")
+              ? (a["prima_dose"] * target[">=5"]) / 100
+              : (a["prima_dose"] * target[">=12"]) / 100
+          )
+          .map((v, i, a) => v - (a[i - 1] || 0))
+          .map((e) => Math.round(e));
+        const immunizzati = res
+          .map((a) =>
+            new Date(a.data) > new Date("2021-12-08")
+              ? (a["seconda_dose"] * target[">=5"]) / 100
+              : (a["seconda_dose"] * target[">=12"]) / 100
+          )
+          .map((v, i, a) => v - (a[i - 1] || 0))
+          .map((e) => Math.round(e));
+        setData({
+          date: date.slice(1),
+          vaccinati: vaccinati.slice(1),
+          immunizzati: immunizzati.slice(1),
+        });
+      }
+      setIsLoading(false);
+    }
+    get();
+  }, [mode, istat, latest]);
 
-    useEffect(() => {
-        async function get() {
-            const res = await fetchData(mode, istat, latest);
-            if(mode === 'incidenza'){
-                setData({
-                    date: res.map(a => format(new Date(a.data), "dd/MM/yy")),
-                    casi: res.map(a => a.casi),
-                    incidenza: res.map(a => a.incidenza)
-                })
-            } else if(mode === 'vaccini'){
-                const target = targets.filter(a => a.pro_com_t.endsWith(istat))[0];
-                const date = res.map(a => format(new Date(a.data), "dd/MM/yy"))
-                const vaccinati = res.map(a => new Date(a.data) > new Date('2021-12-08') ? (a['prima_dose'] * target['>=5'] / 100) : a['prima_dose'] * target['>=12'] / 100).map((v, i, a) => v - (a[i - 1] || 0)).map(e => Math.round(e))
-                const immunizzati = res.map(a => new Date(a.data) > new Date('2021-12-08') ? (a['seconda_dose'] * target['>=5'] / 100) : a['seconda_dose'] * target['>=12'] / 100).map((v, i, a) => v - (a[i - 1] || 0)).map(e => Math.round(e))
-                setData({
-                  date: date.slice(1),
-                  vaccinati: vaccinati.slice(1),
-                  immunizzati: immunizzati.slice(1)
-                })
-            }
-            setIsLoading(false)
-        }
-        get();
-      },[mode, istat, latest]);
+  console.log(data);
 
-
-ChartJS.register(
+  ChartJS.register(
     CategoryScale,
     LinearScale,
     BarElement,
     Title,
     Tooltip,
     Legend
-);
+  );
 
   return (
-      !isLoading&&
+    !isLoading && (
       <div className="h-100 card border-0 rounded">
-        <Bar 
+        <Bar
           data={{
+            min: 0,
             labels: data.date,
-            datasets: mode === 'vaccini' ?
-            [
-              {
-                label: 'Stima variazione vaccinati',
-                backgroundColor: 'rgb(6, 214, 160)',
-                borderColor: 'rgb(6, 214, 160)',
-                borderWidth: 1,
-                hoverBorderWidth: 2,
-                hoverBorderColor: 'lightgray',
-                hoverBackgroundColor: 'rgba(6, 214, 160, 0.5)',
-                data: data.vaccinati
-              },
-              {
-                label: 'Stima variazione immunizzati',
-                backgroundColor: 'rgba(17, 138, 178, 1)',
-                borderColor: 'rgba(17, 138, 178, 1)',
-                borderWidth: 1,
-                hoverBorderWidth: 2,
-                hoverBorderColor: 'lightgray',
-                hoverBackgroundColor: 'rgba(17, 138, 178, 0.5)',
-                data: data.immunizzati
-              }
-            ]
-          :
-            [
-              {
-                label: 'Nuovi casi settimanali',
-                backgroundColor: '#ef476f',
-                borderColor: 'rgba(255,99,132,1)',
-                borderWidth: 1,
-                hoverBorderWidth: 2,
-                hoverBorderColor: 'lightgray',
-                hoverBackgroundColor: '#ef476f',
-                data: data.casi
-              }
-            ]
+            datasets:
+              mode === "vaccini"
+                ? [
+                    {
+                      label: "Stima variazione vaccinati",
+                      backgroundColor: "rgb(6, 214, 160)",
+                      borderColor: "rgb(6, 214, 160)",
+                      borderWidth: 1,
+                      hoverBorderWidth: 2,
+                      hoverBorderColor: "lightgray",
+                      hoverBackgroundColor: "rgba(6, 214, 160, 0.5)",
+                      data: data.vaccinati,
+                    },
+                    {
+                      label: "Stima variazione immunizzati",
+                      backgroundColor: "rgba(17, 138, 178, 1)",
+                      borderColor: "rgba(17, 138, 178, 1)",
+                      borderWidth: 1,
+                      hoverBorderWidth: 2,
+                      hoverBorderColor: "lightgray",
+                      hoverBackgroundColor: "rgba(17, 138, 178, 0.5)",
+                      data: data.immunizzati,
+                    },
+                  ]
+                : [
+                    {
+                      label: "Nuovi casi settimanali",
+                      backgroundColor: "#ef476f",
+                      borderColor: "rgba(255,99,132,1)",
+                      borderWidth: 1,
+                      hoverBorderWidth: 2,
+                      hoverBorderColor: "lightgray",
+                      hoverBackgroundColor: "#ef476f",
+                      data: data.casi,
+                    },
+                  ],
           }}
           options={{
             responsive: true,
             scales: {
-                x: {
-                    grid: {display: false}
-                }
+              x: {
+                grid: { display: false },
+              },
+              y: {
+                min: 0,
+                beginAtZero: true,
+              },
             },
             layout: {
               padding: {
-                  top: 5,
-                  right: 15,
-                  bottom: 5,
-                  left: 15,
-              }
+                top: 5,
+                right: 15,
+                bottom: 5,
+                left: 15,
+              },
             },
             plugins: {
-                legend: {
-                    display: mode === 'vaccini' ? true : false,
+              legend: {
+                display: mode === "vaccini" ? true : false,
               },
-                title: {
-                    display: mode === 'vaccini' ? false : true,
-                    text: 'Nuovi casi settimanali',
-                    font: {
-                      family: 'Roboto',
-                      size: 20,
-                      weight: 'normal'
-                    }
+              title: {
+                display: mode === "vaccini" ? false : true,
+                text: "Nuovi casi settimanali",
+                font: {
+                  family: "Roboto",
+                  size: 20,
+                  weight: "normal",
                 },
+              },
             },
           }}
         />
       </div>
-  )
+    )
+  );
 }
 
--- src/components/Charts/Line.js
+++ src/components/Charts/Line.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import fetchData from '../../lib/fetchData';
-import { format } from 'date-fns';
-import targets from '../../data/targets.json'
+import React, { useState, useEffect } from "react";
+import fetchData from "../../lib/fetchData";
+import { format } from "date-fns";
+import targets from "../../data/targets.json";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -11,152 +11,174 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
+} from "chart.js";
+import { Line } from "react-chartjs-2";
 import annotationPlugin from "chartjs-plugin-annotation";
 
-export default function LineChart({mode, istat, latest}){
+export default function LineChart({ mode, istat, latest }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState({});
 
-    const [isLoading, setIsLoading] = useState(true)
-    const [data, setData] = useState({});
+  useEffect(() => {
+    async function get() {
+      const res = await fetchData(mode, istat, latest);
+      if (mode === "incidenza") {
+        setData({
+          date: res.map((a) => format(new Date(a.data), "dd/MM/yy")),
+          incidenza: res.map((a) => a.incidenza),
+        });
+      } else if (mode === "vaccini") {
+        const target = targets.filter((a) => a.pro_com_t.endsWith(istat))[0];
+        setData({
+          date: res.map((a) => format(new Date(a.data), "dd/MM/yy")),
+          vaccinati: res.map((a) =>
+            a["prima_dose"] === "" ? undefined : a["prima_dose"]
+          ),
+          immunizzati: res.map((a) =>
+            a["prima_dose"] === "" ? undefined : a["seconda_dose"]
+          ),
+          target: res.map((a) =>
+            new Date(a.data) > new Date("2021-12-08")
+              ? target[">=5"]
+              : target[">=12"]
+          ),
+          totale: Array(res.length).fill(target.totale),
+        });
+      }
+      setIsLoading(false);
+    }
+    get();
+  }, [mode, istat, latest]);
 
-    useEffect(() => {
-        async function get() {
-            const res = await fetchData(mode, istat, latest);
-            if(mode === 'incidenza'){
-                setData({
-                    date: res.map(a => format(new Date(a.data), "dd/MM/yy")),
-                    incidenza: res.map(a => a.incidenza)
-                })
-            } else if(mode === 'vaccini'){
-                const target = targets.filter(a => a.pro_com_t.endsWith(istat))[0];
-                setData({
-                    date: res.map(a => format(new Date(a.data), "dd/MM/yy")),
-                    vaccinati: res.map(a => a['prima_dose']),
-                    immunizzati: res.map(a => a['seconda_dose']),
-                    target: res.map(a => new Date(a.data) > new Date('2021-12-08') ? target['>=5'] : target['>=12']),
-                    totale: Array(res.length).fill(target.totale)
-                })
-            }
-            setIsLoading(false)
-        }
-        get();
-      },[mode, istat, latest]);
+  console.log(data);
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-  annotationPlugin
-);
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    annotationPlugin
+  );
 
-const annotation = {
-  type: 'line',
-  borderColor: '#555',
-  borderDash: [3],
-  borderWidth: 1,
-  label: {
-    enabled: true,
-    backgroundColor: '#434343',
-    content: 'target: >=5 anni',
-    position: 'start'
-  },
-  scaleID: 'x',
-  value: '15/12/21'
-}
+  ChartJS.overrides.line.spanGaps = true;
 
-  return(
-      !isLoading&&
-        <div className="h-100 card border-0 rounded">
-          <Line
-            data={{
-                labels: data.date,
-                datasets: mode === 'vaccini' ? 
-                  [
-                      {
-                          label: 'Vaccinati',
-                          data: data.vaccinati,
-                          borderColor: 'rgb(6, 214, 160)',
-                          backgroundColor: 'rgba(6, 214, 160, 0.5)',
-                        },
-                        {
-                          label: 'Immunizzati',
-                          data: data.immunizzati,
-                          borderColor: 'rgb(17, 138, 178)',
-                          backgroundColor: 'rgba(17, 138, 178, 0.5)',
-                        },
-                  ]
-                :
-                  [
+  const annotation = {
+    type: "line",
+    borderColor: "#555",
+    borderDash: [3],
+    borderWidth: 1,
+    label: {
+      enabled: true,
+      backgroundColor: "#434343",
+      content: "target: >=5 anni",
+      position: "start",
+    },
+    scaleID: "x",
+    value: "15/12/21",
+  };
+
+  return (
+    !isLoading && (
+      <div className="h-100 card border-0 rounded">
+        <Line
+          data={{
+            labels: data.date,
+            datasets:
+              mode === "vaccini"
+                ? [
                     {
-                        label: 'Incidenza settimanale',
-                        data: data.incidenza,
-                        borderColor: '#073b4c',
-                        backgroundColor: '#073b4c',
-                      },
-                      {
-                        label: '250 soglia critica',
-                        data: Array(data.date.length).fill(250),
-                        borderDash: [4],
-                        borderColor: '#b7b7b7',
-                        backgroundColor: '#b7b7b7',
-                        pointRadius: 0,
-                      },
-                ]
-            }}
-            options={
-              {
-                responsive: true,
-                borderWidth: 2,
-                scales: {
-                  x: {
-                      grid: {display: false}
-                  },
-                  y: mode === 'vaccini' ? {
+                      label: "Vaccinati",
+                      data: data.vaccinati,
+                      borderColor: "rgb(6, 214, 160)",
+                      backgroundColor: "rgba(6, 214, 160, 0.5)",
+                    },
+                    {
+                      label: "Immunizzati",
+                      data: data.immunizzati,
+                      borderColor: "rgb(17, 138, 178)",
+                      backgroundColor: "rgba(17, 138, 178, 0.5)",
+                    },
+                  ]
+                : [
+                    {
+                      label: "Incidenza settimanale",
+                      data: data.incidenza,
+                      borderColor: "#073b4c",
+                      backgroundColor: "#073b4c",
+                    },
+                    {
+                      label: "250 soglia critica",
+                      data: Array(data.date.length).fill(250),
+                      borderDash: [4],
+                      borderColor: "#b7b7b7",
+                      backgroundColor: "#b7b7b7",
+                      pointRadius: 0,
+                    },
+                  ],
+          }}
+          options={{
+            responsive: true,
+            borderWidth: 2,
+            scales: {
+              x: {
+                grid: { display: false },
+              },
+              y:
+                mode === "vaccini"
+                  ? {
                       suggestedMin: Math.min(...data.vaccinati) - 20,
-                      suggestedMax: Math.max(...data.vaccinati) > 100 ? Math.max(...data.vaccinati)+5 : 100,
+                      suggestedMax:
+                        Math.max(...data.vaccinati) > 100
+                          ? Math.max(...data.vaccinati) + 5
+                          : 100,
                       ticks: {
-                        callback: function(value) {
+                        callback: function (value) {
                           return value + "%";
-                        }
-                      }
-                    } : {
+                        },
+                      },
+                    }
+                  : {
                       suggestedMin: 0,
-                      suggestedMax: 300
-                    }
-                },
-                layout: {
-                  padding: {
-                      top: 15,
-                      right: 15,
-                      bottom: 5,
-                      left: 15,
-                  }
-                },
-                plugins: {
-                  legend: {
-                    position: 'top',
+                      suggestedMax: 300,
+                    },
+            },
+            layout: {
+              padding: {
+                top: 15,
+                right: 15,
+                bottom: 5,
+                left: 15,
+              },
+            },
+            plugins: {
+              legend: {
+                position: "top",
+              },
+              tooltip: {
+                callbacks: {
+                  label: (value) => {
+                    return `${value.dataset.label}: ${value.parsed.y}${
+                      mode === "vaccini" ? "%" : ""
+                    }`;
                   },
-                  tooltip: {
-                    callbacks: {
-                      label: (value) => { 
-                        return `${value.dataset.label}: ${value.parsed.y}${mode === 'vaccini' ? '%' : ''}`
-                      }
-                    }
-                  },
-                  annotation: mode === 'vaccini' ? {
-                    annotations: {
-                      annotation
-                    }
-                  } : null,
                 },
-              }
-            }
-          />
-        </div>
+              },
+              annotation:
+                mode === "vaccini"
+                  ? {
+                      annotations: {
+                        annotation,
+                      },
+                    }
+                  : null,
+            },
+          }}
+        />
+      </div>
+    )
   );
 }
+
